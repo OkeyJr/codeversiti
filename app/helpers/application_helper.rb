985e00cab58b0b98713a232e15a995aba38db505
okey1 <okeyemmanueljr@gmail.com>
2016-04-09 14:11:18 +0100

    title edits and some helper stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #sets the page title on a per-page basis
+  def full_title(page_title = '')
+    base_title = "Codeversiti"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
